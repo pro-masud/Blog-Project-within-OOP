3a88d564e63e91358ccae31988f7dd6e052d1ae2
pro-mannan <promasudbd@gmail.com>
2023-10-16 15:15:49 +0600

    may color theme style options adding this project

--- scripts/css.php
+++ scripts/css.php
@@ -2,10 +2,137 @@
 <link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="style.css">
 <style>
-    /* default color */
+/* default color */
+.headersection {
+    background: #ca932f none repeat scroll 0 0;
+    color: #fff;
+}
+
+.icon a {
+  background: #a56e0a none repeat scroll 0 0;
+}
+
+.searchbtn input[type="text"] {
+  border: 1px solid #A56E0A;
+  background: #FFE07C;
+}
+
+.searchbtn input[type="submit"] {
+  border: 1px solid #9d6602;
+}
+input[type="submit"]{
+    background: #b7801c none repeat scroll 0 0;
+    color: #fff;
+}
+
+.navsection {
+  background: none repeat scroll 0 0 #e6af4b;
+  min-height: 38px;
+}
+
+.navsection ul li {
+  border-left: 1px solid #fdc662;
+  border-right: 1px solid #c18a26;
+}
+
+.samesidebar h2 {
+  background: #e6af4b none repeat scroll 0 0;
+  border-bottom: 2px solid #b7801c;
+  color: #333;
+}
+.samepost h2 a {
+  color: #ac7511;
+  text-decoration: none;
+}
+
+.maincontent {
+  background: #fef4e5 none repeat scroll 0 0;
+}
+
+.sidebar {
+  background: #fef4e5 none repeat scroll 0 0;
+}
+
+.footersection {
+  background: #B7801C;
+}
+
+.footermenu ul li a {
+  color: #E6AF4B;
+}
 
 
-    /* green color */
 
-    
+/* green color */
+.headersection {
+    background: #008000 none repeat scroll 0 0;
+    color: #fff;
+}
+
+.icon a {
+  background: #004E00 none repeat scroll 0 0;
+}
+
+.searchbtn input[type="text"] {
+  border: 1px solid #296E29;
+  width: 200px;
+  background: #3CBC3C;
+  font-size: 16px;
+  height: 20px;
+}
+
+.searchbtn input[type="submit"] {
+  border: 1px solid #004800;
+}
+
+.searchbtn input[type="submit"]:hover {
+  border: 1px solid #004800;
+}
+
+input[type="submit"]{
+    background: #008000 none repeat scroll 0 0;
+    color: #fff;
+}
+
+.searchbtn input[type="submit"]:hover{
+    background: #008000 none repeat scroll 0 0;
+    color: #fff;
+}
+
+.navsection {
+  background: none repeat scroll 0 0 #005700;
+}
+
+.navsection ul li {
+  border-left: 1px solid #005700;
+  border-right: 1px solid #005700;
+}
+
+.samesidebar h2 {
+  background: #008000 none repeat scroll 0 0;
+  border-bottom: 2px solid #60D88B;
+  color: #fff;
+}
+
+.samepost h2 a {
+  color: #008000;
+}
+
+.maincontent {
+    background: #C1FFC0 none repeat scroll 0 0;
+}
+.sidebar{
+    background: #C1FFC0 none repeat scroll 0 0;
+}
+
+.footersection {
+  background: #008000;
+}
+
+.footermenu ul li a {
+  color: #60D88B;
+}
+.footersection p{
+    color: #fff;
+}
 </style>
